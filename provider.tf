a0201ecbfbbefee0b032da433441e958827f717b
Jeison Eslava <jeslava21>
2024-04-20 14:01:00 -0500

    move provider

--- provider.tf
+++ provider.tf
@@ -1,5 +1,15 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.46.0"
+    }
+  }
+  required_version = ">=1.7.5"
+}
 provider "aws" {
     region = "us-east-1"
   # Configuration options
 }
-
